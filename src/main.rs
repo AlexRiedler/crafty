be21269449fdcfec551c1e064d7d13fa39d5b480
Alex Riedler <alex@riedler.ca>
2020-04-08 21:09:44 -0400

    scanning part 1.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 2 {
+        println!("Usage: crafty [script]");
+    } else if args.len() == 2 {
+        let filename = &args[1];
+        run_file(filename);
+    } else {
+        run_prompt();
+    }
+}
+
+fn run_file(filename: &String) {
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    run(contents);
+}
+
+fn run_prompt() {
+    println!("Prompt not implemented yet!");
+}
+
+
+fn run(contents: String) {
 }
